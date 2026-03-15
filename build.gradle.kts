fd6fe41f5bc6739a608d1e27fb69a64999ba5e00
guenter00 <mf1142@outlook.de>
2026-03-15 23:06:05 +0100

    Added workflow-files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("io.papermc.paperweight.userdev") version "2.0.0-SNAPSHOT" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" apply false
     id("dev.architectury.loom") version "1.10-SNAPSHOT" apply false
 }
 
